c7f3d97a23e1f5a471ebe48d985437922a2f6de1
Genadi <myotherneeds@gmail.com>
2016-10-31 19:59:19 +0200

    Adding template for the info page.

---
+++ assignment5/src/public/info/info.controller.js
@@ -0,0 +1,17 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('InfoController', InfoController);
+
+function InfoController() {
+  var infoCtrl = this;
+
+  // infoCtrl.submit = function () {
+  // 	console.log("Done!");
+  //   infoCtrl.completed = true;
+  // };
+
+}
+
+})();
